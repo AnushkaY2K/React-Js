0e3487ec87768ea1469fd0d7ab1498c675c9e316
AnushkaY2K <86051366+AnushkaY2K@users.noreply.github.com>
2022-05-26 14:28:58 +0530

    Initial commit

---
+++ Week 1 Day 1/src/App.js
@@ -0,0 +1,16 @@
+ 
+import Header from './components/Header'
+
+function App() {
+  const name = 'Anushka'
+  const x = false
+  return (
+    <div className="App">
+      <Header />
+    </div>
+    
+  );
+}
+
+
+export default App;
